760a23dea3118ae760437c61526f4a25ec85aefc
poma <alvaropoma2007@gmail.com>
2026-04-16 19:15:06 -0500

    comentario

---
+++ semana-04-python/ejercicio-c#/ejercicio03/ejeercicio03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejeercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese el peso en kg: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese la estatura en metros: ");
+            double estatura = double.Parse(Console.ReadLine());
+
+            //calcular el  IMC usando la funcion de potencia
+            double imc = peso / Math.Pow(estatura, 2); //¿que valor se eleva al cuadrado?
+
+            //redondear a 2 decimales para mostrar
+            double imcred = Math.Round(imc, 2); //cantidad de decimales 
+
+            Console.WriteLine($"IMC: {imcred}");
+
+            //clasificacion por rangos (condicion multiple)
+            if(imc < 18.5)
+            {
+                Console.WriteLine("bajo de peso");
+            }
+            else if (imc >= 18.5 && imc < 25) { //limite superior de 'normal'
+                Console.WriteLine("normal");
+            } else if (imc < 30)
+            {
+                Console.WriteLine("sobrepeso"); //clasificacion correspondiente
+            }
+            else
+            {
+                Console.WriteLine("obesidad");
+            }
+        }
+    }
+}
