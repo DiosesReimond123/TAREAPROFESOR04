517226e3787c3a9ca6f98a3b0726e7640ef2cb3f
Dioses <fubukis670@gmail.com>
2026-04-20 13:51:19 -0500

    agregar archivo

---
+++ JP_TAREA04_PROFESOR/python/Ejercicio1.py/ejercicio03.py
@@ -0,0 +1,17 @@
+peso = float(input("Ingrese el peso en kg: "))
+estatura = float(input("Ingrese la estatura en metros: "))
+
+imc = peso / (estatura ** 2)
+
+imc_red = round(imc, 2)
+
+print(f"IMC: {imc_red}")
+
+if imc < 18.5:
+    print("Bajo peso")
+elif imc < 25:
+    print("Normal")
+elif imc < 30:
+    print("Sobrepeso")
+else:
+    print("Obesidad")
